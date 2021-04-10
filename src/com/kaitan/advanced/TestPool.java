3b5092c130adb03385326dec5b706af950e9d173
Zidiefeng <sunkaitan123@163.com>
2021-04-10 16:08:06 -0400

    Pool & summary

---
+++ src/com/kaitan/advanced/TestPool.java
@@ -0,0 +1,29 @@
+package com.kaitan.advanced;
+
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+
+//测试线程池
+public class TestPool {
+    public static void main(String[] args) {
+        //1.创建线程池
+        ExecutorService service = Executors.newFixedThreadPool(10);
+        service.execute(new MyThread());
+        service.execute(new MyThread());
+        service.execute(new MyThread());
+        service.execute(new MyThread());
+
+        //2. 关闭链接
+        service.shutdown();
+
+
+    }
+}
+
+class MyThread implements Runnable{
+
+    @Override
+    public void run() {
+        System.out.println(Thread.currentThread().getName());
+    }
+}
